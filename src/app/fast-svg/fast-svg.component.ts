fa9a073c07ee7562b638c47a8aa52ef24e76e2ef
Ilya Dubov <ilflask@gmail.com>
2023-10-14 11:10:23 +0500

    Testing performance icons libr

---
+++ src/app/fast-svg/fast-svg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fast-svg',
+  templateUrl: './fast-svg.component.html',
+  styleUrls: ['./fast-svg.component.css']
+})
+export class FastSvgComponent {
+
+}
---
+++ src/app/svg-icon/svg-icon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-icon',
+  templateUrl: './svg-icon.component.html',
+  styleUrls: ['./svg-icon.component.css']
+})
+export class SvgIconComponent {
+
+}
